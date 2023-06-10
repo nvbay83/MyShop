6318141a4a235526feec14de4150518397bebbed
unknown <nvbay83@gmail.com>
2023-06-10 13:11:15 +0700

    add

---
+++ lib/ui/products/user_products_screen.dart
@@ -0,0 +1,72 @@
+// import 'dart:html';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'user_product_list_tile.dart';
+import 'products_manager.dart';
+import '../shared/app_drawer.dart';
+import './edit_product_screen.dart';
+
+class UserProductsScreen extends StatelessWidget {
+  static const routeName = '/user-products';
+  const UserProductsScreen({super.key});
+  Future<void> _refreshProducts(BuildContext context) async {
+    await context.read<ProductsManager>().fetchProducts(true);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final productsManager = ProductsManager();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Your Products'),
+        actions: <Widget>[
+          buildAddButton(context),
+        ],
+      ),
+      drawer: const AppDrawer(),
+      body: FutureBuilder(
+        future: _refreshProducts(context),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+          return RefreshIndicator(
+            child: buildUserProductListView(),
+            onRefresh: () => _refreshProducts(context),
+          );
+        },
+      ),
+    );
+  }
+
+  Widget buildUserProductListView() {
+    return Consumer<ProductsManager>(
+      builder: (ctx, productsManager, child) {
+        return ListView.builder(
+          itemCount: productsManager.itemCount,
+          itemBuilder: (ctx, i) => Column(
+            children: [
+              UserProductListTile(
+                productsManager.items[i],
+              ),
+              const Divider(),
+            ],
+          ),
+        );
+      },
+    );
+  }
+
+  Widget buildAddButton(BuildContext context) {
+    return IconButton(
+      icon: const Icon(Icons.add),
+      onPressed: () {
+        Navigator.of(context).pushNamed(
+          EditProductScreen.routeName,
+        );
+      },
+    );
+  }
+}
